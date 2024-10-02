ac6502794aad22115cbce26721c423d59f5e1840
Mike-Pot <142821437+Mike-Pot@users.noreply.github.com>
2024-10-02 23:20:38 +0300

    Update transport_catalogue.h

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -1,1 +1,43 @@
-// место для вашего кода
+#pragma once
+#include <unordered_map>
+#include <unordered_set>
+#include <deque>
+#include <string>
+#include <string_view>
+#include <vector>
+#include <utility>
+#include <set>
+#include "geo.h"
+
+class TransportCatalogue
+{	
+public:	
+	struct Stats
+	{
+		int stops;
+		int unique_stops;
+		double dist;
+	};
+	TransportCatalogue() = default;
+	void AddStop(std::string&, geo::Coordinates);	
+	void AddRoute(std::string&, std::vector<std::string_view>);
+	bool ShowStat(std::string_view, Stats*) const;
+	bool ShowStop(std::string_view, const std::set<std::string_view>**) const;
+	
+private:
+	struct Stop_
+	{
+		std::string name;
+		geo::Coordinates coord;
+	};
+	struct Bus_
+	{
+		std::string num;
+		std::vector<Stop_*> route;		
+	};
+	std::deque<Bus_> buses_;
+	std::deque<Stop_> stops_;	
+	std::unordered_map<std::string_view, Bus_*> bus_route_key_;
+	std::unordered_map<std::string_view, Stop_*> stop_name_key_;
+	std::unordered_map<Stop_*, std::set<std::string_view>> stops_routes_;	
+};
