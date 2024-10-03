4b153e9ea8237ba157708942cc50d06014dba71b
Mike-Pot <142821437+Mike-Pot@users.noreply.github.com>
2024-10-03 16:30:46 +0300

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -6,3 +6,10 @@
 
 void ParseAndPrintStat(const TransportCatalogue& tansport_catalogue, std::string_view request,
     std::ostream& output);
+namespace reader
+{
+    void PrintBuses(const TransportCatalogue& tansport_catalogue, std::string_view request,
+        std::ostream& output);
+    void PrintStops(const TransportCatalogue& tansport_catalogue, std::string_view request,
+        std::ostream& output);
+}
