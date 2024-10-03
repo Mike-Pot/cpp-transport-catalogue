e31f78cf7ad292adcbf3f7ee162fc88cdb6b128f
Mike-Pot <142821437+Mike-Pot@users.noreply.github.com>
2024-10-03 16:35:41 +0300

    Update transport_catalogue.h

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -7,34 +7,43 @@
 #include <vector>
 #include <utility>
 #include <set>
+#include <optional>
 #include "geo.h"
 
-class TransportCatalogue
-{	
-public:	
+namespace catalogue
+{
+	struct Stop_
+	{
+		std::string name;
+		geo::Coordinates coord;
+	};
+
+	struct Bus_
+	{
+		std::string num;
+		std::vector<Stop_*> route;
+	};
+
 	struct Stats
 	{
 		int stops;
 		int unique_stops;
 		double dist;
 	};
+}
+
+using namespace catalogue;
+
+class TransportCatalogue
+{	
+public:		
 	TransportCatalogue() = default;
-	void AddStop(std::string&, geo::Coordinates);	
-	void AddRoute(std::string&, std::vector<std::string_view>);
-	bool ShowStat(std::string_view, Stats*) const;
-	bool ShowStop(std::string_view, const std::set<std::string_view>**) const;
+	void AddStop(const std::string& stop_name, geo::Coordinates coor);	
+	void AddRoute(const std::string& bus_num, const std::vector<std::string_view>& route);
+	std::optional<Stats> GetStat(std::string_view bus) const;
+	const std::set<std::string_view>* GetBusesWithStop(std::string_view stop) const;
 	
-private:
-	struct Stop_
-	{
-		std::string name;
-		geo::Coordinates coord;
-	};
-	struct Bus_
-	{
-		std::string num;
-		std::vector<Stop_*> route;		
-	};
+private:	
 	std::deque<Bus_> buses_;
 	std::deque<Stop_> stops_;	
 	std::unordered_map<std::string_view, Bus_*> bus_route_key_;
