d9a20e9470386e383db1f5fc6777247b427999fb
Mike-Pot <142821437+Mike-Pot@users.noreply.github.com>
2024-10-03 16:34:34 +0300

    Update transport_catalogue.cpp

--- transport-catalogue/transport_catalogue.cpp
+++ transport-catalogue/transport_catalogue.cpp
@@ -1,20 +1,20 @@
 #include "transport_catalogue.h"
 
-void TransportCatalogue::AddStop(std::string& stop_name, geo::Coordinates coor)
+void TransportCatalogue::AddStop(const std::string& stop_name, geo::Coordinates coor)
 {
-	stops_.push_back({ move(stop_name),coor });
+	stops_.push_back({ stop_name,coor });
 	stop_name_key_.insert({ stops_.back().name, &stops_.back() });	
 	stops_routes_.insert({ &stops_.back(),{} });
 }
 
-void TransportCatalogue::AddRoute(std::string& bus_num, std::vector<std::string_view> route)
+void TransportCatalogue::AddRoute(const std::string& bus_num, const std::vector<std::string_view>& route)
 {
 	std::vector<Stop_*> stops;
 	for (auto stop : route)
 	{
 		stops.push_back(stop_name_key_.at(stop));
 	}
-	buses_.push_back({ move(bus_num),move(stops) });
+	buses_.push_back({ bus_num,move(stops) });
 	bus_route_key_.insert({ buses_.back().num, &buses_.back() });
 	for (auto stop : route)
 	{		
@@ -22,32 +22,33 @@ void TransportCatalogue::AddRoute(std::string& bus_num, std::vector<std::string_
 	}
 }
 
-bool TransportCatalogue::ShowStat(std::string_view bus, Stats* res) const
+std::optional<Stats> TransportCatalogue::GetStat(std::string_view bus) const
 {
-	if (!bus_route_key_.count(bus))
-	{
-		return false;
+	std::optional<Stats> res;	
+	auto it = bus_route_key_.find(bus);	
+	if(it != bus_route_key_.end())
+	{		
+		const std::vector<Stop_*>& stops = it->second->route;
+		int all_stops = stops.size();
+		std::unordered_set<Stop_*> uniques;
+		uniques.insert(stops.begin(), stops.end());		
+		int unq_stops = uniques.size();
+		double dist = 0;
+		for (int i = 0; i < stops.size() - 1; i++)
+		{
+			dist += geo::ComputeDistance((*stops[i]).coord, (*stops[i + 1]).coord);
+		}		
+		res = { all_stops,unq_stops,dist };
 	}	
-	std::vector<Stop_*> stops = bus_route_key_.at(bus)->route;
-	res->stops = stops.size();
-	std::unordered_set<Stop_*> uniques;
-	uniques.insert(stops.begin(), stops.end());
-	res->unique_stops = uniques.size();	
-	double dist = 0;
-	for(int i =0; i < stops.size()-1; i++)
-	{
-		dist += geo::ComputeDistance((*stops[i]).coord, (*stops[i + 1]).coord);
-	}
-	res->dist = dist;
-	return true;
+	return res;
 }
 
-bool TransportCatalogue::ShowStop(std::string_view stop, const std::set<std::string_view>** buses) const
+const std::set<std::string_view>* TransportCatalogue::GetBusesWithStop(std::string_view stop) const
 {	
-	if(!stop_name_key_.count(stop))
+	auto it = stop_name_key_.find(stop);	
+	if(it == stop_name_key_.end())
 	{		
-		return false;
-	}	
-	*buses = &stops_routes_.at(stop_name_key_.at(stop));	
-	return true;
+		return nullptr;
+	}
+	return &stops_routes_.at(it->second);		
 }
