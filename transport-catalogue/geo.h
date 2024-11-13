6033873697b5813e624561ad9b07fec110e7854a
Mike-Pot <142821437+Mike-Pot@users.noreply.github.com>
2024-11-13 14:10:10 +0300

    Update geo.h

--- transport-catalogue/geo.h
+++ transport-catalogue/geo.h
@@ -1,9 +1,7 @@
 #pragma once
 
-#include <cmath>
-
 namespace geo
-{    
+{
     struct Coordinates {
         double lat;
         double lng;
@@ -14,16 +12,5 @@ namespace geo
             return !(*this == other);
         }
     };
-
-    inline double ComputeDistance(Coordinates from, Coordinates to) {
-        using namespace std;
-        double earth_radius = 6371000;
-        if (from == to) {
-            return 0;
-        }
-        static const double dr = 3.1415926535 / 180.;
-        return acos(sin(from.lat * dr) * sin(to.lat * dr)
-            + cos(from.lat * dr) * cos(to.lat * dr) * cos(abs(from.lng - to.lng) * dr))
-            * earth_radius;
-    }
+    double ComputeDistance(Coordinates from, Coordinates to);
 }
