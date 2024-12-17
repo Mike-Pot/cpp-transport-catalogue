c80e2dd2f672f13101810a38925d75bac1cccf6d
Mike-Pot <142821437+Mike-Pot@users.noreply.github.com>
2024-12-17 22:16:29 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -5,23 +5,13 @@
 using namespace std;
 
 int main()
-{
-    /*
-    graph::DirectedWeightedGraph<double> g {3};
-    g.AddEdge({ 0, 1, 10 });
-    graph::Router r{g};
-    r.BuildRoute(1, 0);
-    */
-    
+{    
     catalogue::TransportCatalogue cat;
     JsonReader inp(cin);
     inp.LoadDB(cat);    
     renderer::MapRenderer map_rend;    
     inp.GetRenderSettings(map_rend);
     rout::TransportRouter router = inp.GetRoutSettings(cat);
- //   cout << router.kkk();
-  //  auto a = router.GetRoute(2, 6);
     inp.PrintDB({ cat, map_rend, router }, cout);   
-    return 0;
-    
+    return 0;    
 }
